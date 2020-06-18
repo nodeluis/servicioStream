d67eb8379f38eda967dc66bd66d24b8ecd070bbc
luis <lruise.1@gmail.com>
2020-06-17 23:02:56 -0400

    cors 3

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var pruebaRouter = require('./routes/api/prueba');
 var app = express();
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://app-sis719.herokuapp.com"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "/*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
