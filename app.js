afa87bade1423a672d66718e1755e25441b28b16
luis <lruise.1@gmail.com>
2020-06-17 22:59:53 -0400

    cors 2

--- app.js
+++ app.js
@@ -15,7 +15,11 @@ var pruebaRouter = require('./routes/api/prueba');
 
 var app = express();
 
-app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://app-sis719.herokuapp.com"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
